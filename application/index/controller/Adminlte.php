e8a017070fa4612ca762a9f7a0f1b8708eaf1e96
shuangdeyu <xiaof_for@163.com>
2019-02-28 00:52:50 +0800

    修改后台管理

--- application/index/controller/Adminlte.php
+++ application/index/controller/Adminlte.php
@@ -166,4 +166,189 @@ class Adminlte extends Controller
         //返回到父页面
         echo '<script>history.go(-2);</script>';
     }
+
+    /*---------------------------------------------------------------*
+     *--------------------------文章相关-----------------------------*
+     *---------------------------------------------------------------*/
+    const moviePhotoAddr1 = './public/static/images/movie/'; //文件夹的地址形式
+    const moviePhotoAddr2 = '__IMG__/movie/'; //写入数据库的地址形式
+    /**
+     * 显示影评列表
+     */
+    public function movie_show(){
+        $movie = Db::name('movie')->order('id desc')->paginate(15);
+
+        $this->assign('movie',$movie);
+        return $this->fetch();
+    }
+
+    /**
+     * 影评详细信息
+     */
+    public function movie_info(){
+        $id = input('get.id');
+        $movie = Db::name('movie')->where('id',$id)->find();
+
+        $this->assign('movie',$movie);
+        return $this->fetch();
+    }
+
+    /**
+     * 添加影评界面
+     */
+    public function movie_add(){
+        $m = input('get.m');
+
+        $this->assign('is_m',$m);
+        return $this->fetch();
+    }
+
+    /**
+     * 添加新影评
+     */
+    public function movie_save(){
+        //接收表单
+        $title = input('post.title');
+        $content = input('post.content');
+        $is_markdown = input('post.is_markdown');
+
+        //当前时间，年月日
+        $time = date('Y-m-d');
+
+        /*上传到七牛*/
+        $Upload = new Upload();
+        $pic_url = $Upload->one(request()->file('Photo'));
+
+        if($pic_url) {
+            //先添加进数据库
+            $Movie = Db::name('movie');
+            $data  = array();
+            $data['title']   = $title;
+            $data['content'] = $content;
+            $data['time']    = $time;
+            $data['cover']   = $pic_url;
+            $data['is_markdown'] = $is_markdown;
+            $add = $Movie->insert($data);
+        }
+
+        //返回到父页面
+        $url = url('index/adminlte/movie_show');
+        echo "<script>window.location.href='$url';</script>";
+    }
+
+    /**
+     * 删除影评
+     */
+    public function movie_del(){
+        $movieId = input('post.mid');
+
+        $where['id'] = $movieId;
+
+        //先获取图片路径，删除图片
+        $data = Db::name('movie')->where($where)->field('cover')->find();
+
+        if($data['cover']) {
+            /*删除本地图片*/
+            /*$dir = substr(strrchr($data['cover'], '/'), 1);
+            $dir = self::moviePhotoAddr1 . $dir;
+			if(is_file($dir)){
+				unlink($dir);
+			}*/
+
+            /*删除七牛图片*/
+            $Upload = new Upload();
+            $delete = $Upload->delete($data['cover']);
+        }
+
+        $del = Db::name('movie')->where($where)->delete();
+
+        return true;
+    }
+
+    /**
+     * 编辑影评显示
+     */
+    public function movie_edit(){
+        $id = input('get.id');
+        $is_markdown = input('get.is_m');
+        $movie = Db::name('movie')->where('id',$id)->find();
+
+        $this->assign('movie',$movie);
+        $this->assign('is_m',$is_markdown);
+        return $this->fetch();
+    }
+
+    /**
+     * 编辑影评操作
+     */
+    public function movie_update(){
+        $movie_id = input('post.mid');
+        $title = input('post.title');
+        $cover = input('post.cover');
+        $content = input('post.content');
+
+        //当前时间，年月日
+        $time = date('Y-m-d');
+
+        //判断是否有更新封面图片
+        if(isset($_FILES['Photo']) && $_FILES['Photo']['name'] != ''){
+            //先删除原来的图片
+            /*$deldir = substr(strrchr($cover, '/'), 1);
+            $deldir = self::moviePhotoAddr1.$deldir;
+			if(is_file($deldir)){
+				unlink($deldir);
+			}*/
+            $Upload = new Upload();
+            $delete = $Upload->delete($cover);
+
+            //图片名称
+            /*$filename = uniqid();
+            //获取图片扩展名
+            $exname = strrchr($_FILES['Photo']['name'], '.');
+
+            //检查图片文件夹是否存在
+            if(!is_dir(self::moviePhotoAddr1)){
+                mkdir(self::moviePhotoAddr1, 0777);
+            }
+
+            //图片总路径-上传文件
+            $dir1 = self::moviePhotoAddr1.$filename.$exname;
+            //图片总路径-写入数据库
+            $dir2 = self::moviePhotoAddr2.$filename.$exname;*/
+
+            /*上传到七牛*/
+            $Upload = new Upload();
+            $pic_url = $Upload->one(request()->file('Photo'));
+
+            if($pic_url) {
+                //先更新数据库
+                $Movie = Db::name('movie');
+                $data = array();
+                $data['title']   = $title;
+                $data['content'] = $content;
+                $data['time']    = $time;
+                $data['cover']   = $pic_url;
+                $up = $Movie->where('id=' . $movie_id)->update($data);
+            }
+
+            //判断是否更新成功，再将图片传入文件夹
+            /*if(is_numeric($up)){
+                //将缓存中的图片移到正确位置
+                move_uploaded_file($_FILES['Photo']['tmp_name'], $dir1);
+            }*/
+        }else{
+            //没有更新封面图片
+            $Movie = Db::name('movie');
+
+            $data = array();
+            $data['title'] = $title;
+            $data['content'] = $content;
+            $data['time'] = $time;
+
+            $up = $Movie->where('id='.$movie_id)->update($data);
+        }
+
+        //返回父页面
+        echo "<script>parent.window.location.reload();</script>";
+    }
 }
