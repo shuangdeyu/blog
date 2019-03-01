1b477ef6f808e8b576857e8524a159d972f35987
jiangyj <jiangyj@zjmax.com>
2019-03-01 18:25:30 +0800

    相册相关管理更新

--- application/index/controller/Adminlte.php
+++ application/index/controller/Adminlte.php
@@ -18,7 +18,6 @@ use think\Session;
  * 本来是基于Adminlte框架的后台，先改成基于Gentelella
  * @package app\index\controller
  */
-
 class Adminlte extends Controller
 {
     public function __construct(Request $request = null)
@@ -110,23 +109,26 @@ class Adminlte extends Controller
     /**
      * 删除微博
      */
-    public function weibo_del(){
+    public function weibo_del()
+    {
         $wid = input('post.wid');
-        Db::name('weibo')->where('id',$wid)->delete();
+        Db::name('weibo')->where('id', $wid)->delete();
         return true;
     }
 
     /**
      * 添加微博界面
      */
-    public function weibo_add(){
+    public function weibo_add()
+    {
         return $this->fetch();
     }
 
     /**
      * 保存添加
      */
-    public function weibo_save(){
+    public function weibo_save()
+    {
         $name = input('post.name');
         $link = input('post.link');
         $content = input('post.content');
@@ -144,19 +146,21 @@ class Adminlte extends Controller
     /**
      * 微博编辑界面
      */
-    public function weibo_info(){
+    public function weibo_info()
+    {
         $id = input('get.id');
 
-        $weibo = Db::name('weibo')->where('id',$id)->find();
+        $weibo = Db::name('weibo')->where('id', $id)->find();
 
-        $this->assign('weibo',$weibo);
+        $this->assign('weibo', $weibo);
         return $this->fetch();
     }
 
     /**
      * 编辑微博
      */
-    public function weibo_edit(){
+    public function weibo_edit()
+    {
         $id = input('post.id');
         $name = input('post.name');
         $link = input('post.link');
@@ -165,7 +169,7 @@ class Adminlte extends Controller
         $data['name'] = $name;
         $data['link'] = $link;
         $data['content'] = $content;
-        Db::name('weibo')->where('id',$id)->update($data);
+        Db::name('weibo')->where('id', $id)->update($data);
 
         //返回到父页面
         echo '<script>history.go(-2);</script>';
@@ -176,41 +180,46 @@ class Adminlte extends Controller
      *---------------------------------------------------------------*/
     const moviePhotoAddr1 = './public/static/images/movie/'; //文件夹的地址形式
     const moviePhotoAddr2 = '__IMG__/movie/'; //写入数据库的地址形式
+
     /**
      * 显示影评列表
      */
-    public function movie_show(){
+    public function movie_show()
+    {
         $movie = Db::name('movie')->order('id desc')->paginate(15);
 
-        $this->assign('movie',$movie);
+        $this->assign('movie', $movie);
         return $this->fetch();
     }
 
     /**
      * 影评详细信息
      */
-    public function movie_info(){
+    public function movie_info()
+    {
         $id = input('get.id');
-        $movie = Db::name('movie')->where('id',$id)->find();
+        $movie = Db::name('movie')->where('id', $id)->find();
 
-        $this->assign('movie',$movie);
+        $this->assign('movie', $movie);
         return $this->fetch();
     }
 
     /**
      * 添加影评界面
      */
-    public function movie_add(){
+    public function movie_add()
+    {
         $m = input('get.m');
 
-        $this->assign('is_m',$m);
+        $this->assign('is_m', $m);
         return $this->fetch();
     }
 
     /**
      * 添加新影评
      */
-    public function movie_save(){
+    public function movie_save()
+    {
         //接收表单
         $title = input('post.title');
         $content = input('post.content');
@@ -223,14 +232,14 @@ class Adminlte extends Controller
         $Upload = new Upload();
         $pic_url = $Upload->one(request()->file('Photo'));
 
-        if($pic_url) {
+        if ($pic_url) {
             //先添加进数据库
             $Movie = Db::name('movie');
-            $data  = array();
-            $data['title']   = $title;
+            $data = array();
+            $data['title'] = $title;
             $data['content'] = $content;
-            $data['time']    = $time;
-            $data['cover']   = $pic_url;
+            $data['time'] = $time;
+            $data['cover'] = $pic_url;
             $data['is_markdown'] = $is_markdown;
             $add = $Movie->insert($data);
         }
@@ -243,7 +252,8 @@ class Adminlte extends Controller
     /**
      * 删除影评
      */
-    public function movie_del(){
+    public function movie_del()
+    {
         $movieId = input('post.mid');
 
         $where['id'] = $movieId;
@@ -251,7 +261,7 @@ class Adminlte extends Controller
         //先获取图片路径，删除图片
         $data = Db::name('movie')->where($where)->field('cover')->find();
 
-        if($data['cover']) {
+        if ($data['cover']) {
             /*删除七牛图片*/
             $Upload = new Upload();
             $delete = $Upload->delete($data['cover']);
@@ -265,20 +275,22 @@ class Adminlte extends Controller
     /**
      * 编辑影评显示
      */
-    public function movie_edit(){
+    public function movie_edit()
+    {
         $id = input('get.id');
         $is_markdown = input('get.is_m');
-        $movie = Db::name('movie')->where('id',$id)->find();
+        $movie = Db::name('movie')->where('id', $id)->find();
 
-        $this->assign('movie',$movie);
-        $this->assign('is_m',$is_markdown);
+        $this->assign('movie', $movie);
+        $this->assign('is_m', $is_markdown);
         return $this->fetch();
     }
 
     /**
      * 编辑影评操作
      */
-    public function movie_update(){
+    public function movie_update()
+    {
         $movie_id = input('post.mid');
         $title = input('post.title');
         $cover = input('post.cover');
@@ -288,7 +300,7 @@ class Adminlte extends Controller
         $time = date('Y-m-d');
 
         //判断是否有更新封面图片
-        if(isset($_FILES['Photo']) && $_FILES['Photo']['name'] != ''){
+        if (isset($_FILES['Photo']) && $_FILES['Photo']['name'] != '') {
             $Upload = new Upload();
             $delete = $Upload->delete($cover);
 
@@ -296,17 +308,17 @@ class Adminlte extends Controller
             $Upload = new Upload();
             $pic_url = $Upload->one(request()->file('Photo'));
 
-            if($pic_url) {
+            if ($pic_url) {
                 //先更新数据库
                 $Movie = Db::name('movie');
                 $data = array();
-                $data['title']   = $title;
+                $data['title'] = $title;
                 $data['content'] = $content;
-                $data['time']    = $time;
-                $data['cover']   = $pic_url;
+                $data['time'] = $time;
+                $data['cover'] = $pic_url;
                 $up = $Movie->where('id=' . $movie_id)->update($data);
             }
-        }else{
+        } else {
             //没有更新封面图片
             $Movie = Db::name('movie');
 
@@ -315,10 +327,245 @@ class Adminlte extends Controller
             $data['content'] = $content;
             $data['time'] = $time;
 
-            $up = $Movie->where('id='.$movie_id)->update($data);
+            $up = $Movie->where('id=' . $movie_id)->update($data);
         }
 
         //返回父页面
         echo "<script>history.go(-2);</script>";
+    }
+
+    /*---------------------------------------------------------------*
+     *--------------------------图文相关-----------------------------*
+     *---------------------------------------------------------------*/
+    const syPhotoAddr1 = './public/static/images/zuopin/'; //文件夹的地址形式
+    const syPhotoAddr2 = '__IMG__/zuopin/'; //写入数据库的地址形式
+
+    /**
+     * 显示图文列表
+     */
+    public function photo_show()
+    {
+        $type = input('get.type');
+        if ($type == '') $type = 0;
+
+        $photo = Db::name('photo')->where('type', $type)->select();
+
+        $this->assign('photo', $photo);
+        $this->assign('type', $type);
+        return $this->fetch();
+    }
+
+    /**
+     * 添加图文
+     */
+    public function photo_add()
+    {
+        $type = input('get.type');
+        if ($type == '') $type = 0;
+
+        $this->assign('type', $type);
+        return $this->fetch();
+    }
+
+    /**
+     * 添加图文操作
+     */
+    public function photo_save()
+    {
+        $type = input('post.type'); //类型，0单图，1、2多图
+        $content = input('post.content');
+
+        if (!request()->file('Photo')) {
+            $this->error("图片未选择", null, "", 1);
+        }
+
+        if ($type == 0) {
+            /*上传到七牛*/
+            $Upload = new Upload();
+            $addr = $Upload->one(request()->file('Photo'));
+
+            if ($addr) {
+                $data_photo['content'] = $content;
+                $data_photo['type'] = $type;
+                $photo_id = Db::name('photo')->insertGetId($data_photo);
+
+                $data_img['pic_url'] = $addr;
+                $data_img['target_id'] = $photo_id;
+                $data_img['target'] = 0;
+                Db::name('images')->insertGetId($data_img);
+            }
+        } else {
+            $data_photo['content'] = $content;
+            $data_photo['type'] = $type;
+            $photo_id = Db::name('photo')->insertGetId($data_photo);
+
+            $files = request()->file('Photo');
+            foreach ($files as $file) {
+                /*上传到七牛*/
+                $Upload = new Upload();
+                $addr = $Upload->one($file);
+
+                if ($addr) {
+                    $data_img['pic_url'] = $addr;
+                    $data_img['target_id'] = $photo_id;
+                    $data_img['target'] = 0;
+                    Db::name('images')->insertGetId($data_img);
+                }
+            }
+        }
+        $url = url('index/adminlte/photo_show');
+        echo "<script>window.location.href='$url';</script>";
+    }
+
+    /**
+     * 删除图文
+     */
+    public function photo_del()
+    {
+        $pid = input('post.pid');
+
+        Db::name('photo')->where('id', $pid)->delete();
+
+        $img = Db::name('images')->where('target_id', $pid)->where('target', 0)->select();
+        foreach ($img as $value) {
+            $Upload = new Upload();
+            $Upload->delete($value['pic_url']);
+        }
+        Db::name('images')->where('target_id', $pid)->where('target', 0)->delete();
+
+        return true;
+    }
+
+    /**
+     * 编辑图文显示
+     */
+    public function photo_edit()
+    {
+        $id = input('get.id');
+        $type = input('get.type');
+
+        $photo = Db::name('photo')->where('id', $id)->find();
+        $images = Db::name('images')->where('target_id', $id)->where('target', 0)->select();
+
+        $this->assign('photo', $photo);
+        $this->assign('img', $images);
+        $this->assign('type', $type);
+        return $this->fetch();
+    }
+
+    /**
+     * 编辑图文操作
+     */
+    public function photo_update()
+    {
+        $type = input('post.type');
+        $id = input('post.id');
+        $content = input('post.content');
+
+        //修改photo表数据
+        $data['content'] = $content;
+        Db::name('photo')->where('id', $id)->update($data);
+
+        $is_up = 0;
+        if ($type == 0) {
+            if ($_FILES['Photo']['error'] == 0) $is_up = 1; //有更新图片
+        } else {
+            if ($_FILES['Photo']['error'][0] == 0) $is_up = 1; //没更新图片
+        }
+        if ($is_up) {
+            //先删除原来的图片
+            $img = Db::name('images')->where('target_id', $id)->where('target', 0)->select();
+            foreach ($img as $value) {
+                $Upload = new Upload();
+                $Upload->delete($value['pic_url']);
+            }
+            Db::name('images')->where('target_id', $id)->where('target', 0)->delete();
+
+            //添加更新图片
+            if ($type == 0) {
+                $file = request()->file('Photo');
+                $Upload = new Upload();
+                $addr = $Upload->one($file);
+
+                if ($addr) {
+                    $data_img['pic_url'] = $addr;
+                    $data_img['target_id'] = $id;
+                    $data_img['target'] = 0;
+                    Db::name('images')->insertGetId($data_img);
+                }
+            } else {
+                $files = request()->file('Photo');
+                foreach ($files as $file) {
+                    $Upload = new Upload();
+                    $addr = $Upload->one($file);
+
+                    if ($addr) {
+                        $data_img['pic_url'] = $addr;
+                        $data_img['target_id'] = $id;
+                        $data_img['target'] = 0;
+                        Db::name('images')->insertGetId($data_img);
+                    }
+                }
+            }
+        }
+        echo "<script>history.go(-2)</script>";
+    }
+
+    /*---------------------------------------------------------------*
+     *--------------------------相册相关-----------------------------*
+     *---------------------------------------------------------------*/
+    const xcPhotoAddr1 = './public/static/images/photo/'; //文件夹的地址形式
+    const xcPhotoAddr2 = '__IMG__/photo/'; //写入数据库的地址形式
+
+    /**
+     * 显示图片列表
+     */
+    public function images_show(){
+        $img = Db::name('images')->where('target',1)->select();
+
+        $this->assign('img',$img);
+        return $this->fetch();
+    }
+
+    /**
+     * 添加图片显示
+     */
+    public function images_add(){
+        return $this->fetch();
+    }
+
+    /**
+     * 添加图片操作
+     */
+    public function images_save(){
+        $files = request()->file('Photo');
+        foreach($files as $file){
+            $Upload = new Upload();
+            $addr = $Upload->one($file);
+
+            if($addr) {
+                $data_img['pic_url']   = $addr;
+                $data_img['target_id'] = 0;
+                $data_img['target']    = 1;
+                Db::name('images')->insertGetId($data_img);
+            }
+        }
+        echo "<script>parent.window.location.reload();</script>";
+    }
+
+    /**
+     * 删除图片
+     */
+    public function images_del(){
+        $imgId = input('post.delImg/a');
+        foreach($imgId as $value){
+            $img = Db::name('images')->where('id',$value)->find();
+
+            $Upload = new Upload();
+            $Upload->delete($img['pic_url']);
+
+            Db::name('images')->where('id',$value)->delete();
+        }
+        echo "<script>self.location=document.referrer;</script>";
     }
 }
