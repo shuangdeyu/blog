81132416bbcf76360593018b41c604350fc370f4
shuangdeyu <xiaof_for@163.com>
2019-03-15 00:43:32 +0800

    更新评论阅读状态

--- application/common.php
+++ application/common.php
@@ -102,12 +102,12 @@ function curlGet($url, $headers = array())
  * @param $data
  * @return mixed
  */
-function putUrl($url, $data)
+function putUrl($url, $data, $headers = array())
 {
     $data = json_encode($data);
     $ch = curl_init(); //初始化CURL句柄
     curl_setopt($ch, CURLOPT_URL, $url); //设置请求的URL
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type:application/json'));
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); //设为TRUE把curl_exec()结果转化为字串，而不是直接输出
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT"); //设置请求方式
     curl_setopt($ch, CURLOPT_POSTFIELDS, $data);//设置提交的字符串
