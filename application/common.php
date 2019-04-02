af9686fe4522883575dc00de4b783b74461397ea
jiangyj <jiangyj@zjmax.com>
2019-04-02 17:59:53 +0800

    豆瓣电影实验站更新

--- application/common.php
+++ application/common.php
@@ -145,3 +145,16 @@ function getTagsName($tags = '') {
     $data = $movie->tags_name($tags);
     return $data;
 }
+
+/**
+ * 图片base64编码
+ * @param $image_file
+ * @return string
+ */
+function base64EncodeImage ($image_file) {
+    $base64_image = '';
+    $image_info = getimagesize($image_file);
+    $image_data = fread(fopen($image_file, 'r'), filesize($image_file));
+    $base64_image = 'data:' . $image_info['mime'] . ';base64,' . chunk_split(base64_encode($image_data));
+    return $base64_image;
+}
--- application/index/controller/Photo.php
+++ application/index/controller/Photo.php
@@ -64,7 +64,7 @@ class Photo extends Controller
         return $this->fetch();
     }
 
-    // 豆瓣电实验验站点
+    /************************************************* 豆瓣电影实验站点 *************************************************/
     public function douban_film() {
         $tag = trim(input('get.tag', ''));
         $order = trim(input('get.order', 'release')); // rank 评分，recommend 评价数(热度), release 上映时间
@@ -100,54 +100,68 @@ class Photo extends Controller
                 'query' => ['tag' => $tag, 'order' => $order],
             ]);
 
-
-
-        $url = 'https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2541901817.jpg';
-        $img = "http://qiniucdn.shuangdeyu.com/blog9f6ac201903181512463831.jpg";
-        $path = ROOT_PATH ."public".DS."static".DS."images".DS."testsst".DS;
-//        echo $path;
-        if (!is_dir($path)) {
-            mkdir($path, 0777, true);
-        }
-
-        if(empty($img)){
-            return false;
-        }
-        //获取图片信息大小
-        $imgSize = getImageSize($img);
-        if(!in_array($imgSize['mime'],array('image/jpg', 'image/gif', 'image/png', 'image/jpeg'),true)){
-            return false;
-        }
-        //获取后缀名
-        $_mime = explode('/', $imgSize['mime']);
-        $_ext = '.'.end($_mime);
-        if(empty($fileName)){  //生成唯一的文件名
-            $fileName = uniqid(time(),true).$_ext;
-        }
-        //开始攫取
-        ob_start();
-        readfile($img);
-        $imgInfo = ob_get_contents();
-        ob_end_clean();
-
-        $fp = fopen($path.$fileName, 'a');
-        $imgLen = strlen($imgInfo);    //计算图片源码大小
-        $_inx = 1024;   //每次写入1k
-        $_time = ceil($imgLen/$_inx);
-        for($i=0; $i<$_time; $i++){
-            fwrite($fp,substr($imgInfo, $i*$_inx, $_inx));
-        }
-        fclose($fp);
-        echo $path.$fileName;
-
-        
-
         $tag == '' ? $tag = '全部' : $tag;
         $this->assign('tags', $tags);
-//        $this->assign('tags', $new_tags);
         $this->assign('list', $list);
         $this->assign('tag', $tag);
         $this->assign('order', $order);
-//        return $this->fetch();
+        return $this->fetch();
     }
+
+    // 图片处理成base64
+    public function douban_photo_base64() {
+        $is_close = 1;
+        if (!$is_close) {
+            $path = ROOT_PATH . "public" . DS . "static" . DS . "images" . DS . "testsst" . DS;
+            if (!is_dir($path)) {
+                mkdir($path, 0777, true);
+            }
+            // 获取记录
+            $m = 41; // 更新的进度，已更新的值
+            $start = 20 * $m;
+            $limit = 20;
+            $conf = Config::get('database')['db_config2'];
+            $list = Db::connect($conf)->table('q_movie')->limit($start, $limit)->select();
+            foreach ($list as $k => $v) {
+                $film_img = $v['img'];
+
+                if (empty($film_img)) {
+                    continue;
+                }
+                //获取图片信息大小
+                $imgSize = getImageSize($film_img);
+                if (!in_array($imgSize['mime'], array('image/jpg', 'image/gif', 'image/png', 'image/jpeg'), true)) {
+                    continue;
+                }
+                //获取后缀名
+                $_mime = explode('/', $imgSize['mime']);
+                $_ext = '.' . end($_mime);
+                $fileName = 'douban_film_img-id-' . $v['id'] . $_ext;
+                //开始攫取
+                ob_start();
+                readfile($film_img);
+                $imgInfo = ob_get_contents();
+                ob_end_clean();
+
+                $fp = fopen($path . $fileName, 'a');
+                $imgLen = strlen($imgInfo);    //计算图片源码大小
+                $_inx = 1024;   //每次写入1k
+                $_time = ceil($imgLen / $_inx);
+                for ($i = 0; $i < $_time; $i++) {
+                    fwrite($fp, substr($imgInfo, $i * $_inx, $_inx));
+                }
+                fclose($fp);
+
+                $new_img = $path . $fileName;
+                $base64_img = base64EncodeImage($new_img);
+                // 更新图片为base64
+                $conf = Config::get('database')['db_config2'];
+                $up = Db::connect($conf)->table('q_movie')->where('id=' . $v['id'])->update(array(
+                    'img' => $base64_img
+                ));
+            }
+            echo 'success' . $m;
+        }
+    }
+    /************************************************* 豆瓣电影实验站点 *************************************************/
 }
